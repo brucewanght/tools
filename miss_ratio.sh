4d64c4bd1dea1b55ec70ce47f7919c04acf37276
bruce <brucewanght@qq.com>
2018-02-28 11:30:53 -0600

    periodically collect the miss ratio

--- miss_ratio.sh
+++ miss_ratio.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+if [ "$1" == "" ];then 
+	echo "please input dm-cache user id"
+	exit 1
+fi
 
-hits=$(dmsetup status my_cache1|cut -d" " -f8)
-misses=$(dmsetup status my_cache1|cut -d" " -f9)
-cbk_size=$(dmsetup status my_cache1|cut -d" " -f6)
-cbks=$(dmsetup status my_cache1|cut -d" " -f7|cut -d"/" -f2)
-echo "hits=$hits, misses=$misses, cbk_size=$cbk_size, cbks=$cbks"
-#cache_size in GB
-cache_size=$(awk 'BEGIN{print '$cbks'*'$cbk_size'/2048/1024}')
-miss_ratio=$(awk 'BEGIN{print '$misses'/('$misses' + '$hits')}')
+if [ "$2" == "" ];then 
+	echo "please input mrc log file name"
+	exit 1
+fi 
 
-echo "$cache_size, $misses, $hits, $miss_ratio" >>$1
+if [ "$3" == "" ];then 
+	echo "default collect interval is 2 seconds"
+	int=2
+else
+	int=$3
+fi 
+while true;do
+sleep $int 
+	hits=$(dmsetup status my_cache$1|cut -d" " -f8)
+	misses=$(dmsetup status my_cache$1|cut -d" " -f9)
+	cbk_size=$(dmsetup status my_cache$1|cut -d" " -f6)
+	cbks=$(dmsetup status my_cache$1|cut -d" " -f7|cut -d"/" -f2)
+	#cache_size in GB
+	cache_size=$(awk 'BEGIN{print '$cbks'*'$cbk_size'/2048/1024}')
+	miss_ratio=$(awk 'BEGIN{print '$misses'/('$misses' + '$hits')}')
+	echo "$cache_size, $misses, $hits, $miss_ratio"
+	echo "$cache_size, $misses, $hits, $miss_ratio" >>$2
+done
