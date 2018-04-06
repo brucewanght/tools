ab65c966bf58324f995f290d14ffb8eb00ae5d31
bruce <brucewanght@qq.com>
2018-04-05 19:26:21 -0500

    python script for plotting performance curves

---
+++ linechart.py
@@ -0,0 +1,71 @@
+#coding=utf-8
+"""
+========
+Note:
+1.This script is used to read data from csv files and produce a barchar that reflect
+   the comparisonof  these data.
+2.Every row of csv file represent a group data 
+========
+
+A bar plot with errorbars and height labels on individual bars
+"""
+import csv
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+autoread: read data form  file to list 
+'''
+def autoread(filename):
+    cache_size=[]
+    miss_ratio=[]
+    csvfile_in = file(filename, 'rb')
+    reader =csv.reader(csvfile_in)
+    for line in reader:
+        csize=int(line[5])
+        mr=float(line[6])
+        cache_size.append(csize)
+        miss_ratio.append(mr)
+    csvfile_in.close()
+    return cache_size, miss_ratio
+
+'''
+plotdata: plot data to barchart
+'''
+def plotline(cache_size,miss_ratio,name):
+    fig = plt.figure(dpi=300, figsize=(8, 7))
+    max_x=max(cache_size)
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(cache_size, miss_ratio, 'r-',linewidth=6.0)
+    
+    plt.ylim(0, 1.0)
+    plt.xlim(1,max_x)
+    plt.grid()
+
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+
+    plt.xlabel('Cache Size(GB)', fontsize=22)
+    plt.ylabel('Miss Ratio', fontsize=22)
+    #ax.set_title(name)
+
+    #save figure as fname
+    plt.savefig(name+'.pdf')  #save figure as fname
+    plt.savefig(name+'.png')  #save figure as fname
+    #plt.show()           #show the figure
+    plt.close()
+
+'''
+main function: scan the files in current directory, find scv files and plot their data
+'''
+if __name__=="__main__":
+    filenames=os.listdir(os.getcwd())  #get file names in current directory
+    for fn in filenames:
+        if fn.endswith(".mrc"):
+            cache_size=[]
+            miss_ratio=[]
+            cache_size,miss_ratio=autoread(fn)
+            name=fn.split(".")[0]
+            print name
+            plotline(cache_size,miss_ratio,name)
